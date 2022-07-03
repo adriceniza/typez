c157ac102471f0fa532da0c207e6b7d13fe582d2
Adrian C. Agudin <ceniiziienta@gmail.com>
2022-07-03 02:17:27 +0100

    leaderboards page

---
+++ pages/leaderboards.tsx
@@ -0,0 +1,11 @@
+import ModalTerminal from '@components/Terminal/modalTerminal'
+import React from 'react'
+
+export default function leaderboards() {
+    return (<>
+        <div>Leaderboards</div>
+        <ModalTerminal />
+    </>
+
+    )
+}
